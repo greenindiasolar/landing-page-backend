08eae2b2f963801cfe1915aa76c601ad559c5716
amitsunda1 <amitsunda14374@gmail.com>
2026-01-22 16:50:25 +0530

    hostinger issue

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-    port: process.env.PORT || 3001,
+    port: process.env.PORT ,
     resendApiKey: process.env.RESEND_API_KEY || '',
     adminEmail: process.env.ADMIN_EMAIL || 'admin@example.com',
     googleScriptConfigUrl: process.env.GOOGLE_SCRIPT_CONFIG_URL || '',
